cc490d5509f74bf63c26d97c287aadfbb8309dd9
edircabsan <edircabsan@gmail.com>
2022-05-31 14:12:55 -0300

    First Version

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='my',  
+     version='0.1',
+     scripts=['my'] ,
+     author="Edircabsan",
+     author_email="astner-br@gmail.com",
+     description="A test package",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/edircabsan/my",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
